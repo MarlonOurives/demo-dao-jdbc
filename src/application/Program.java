bad7fa42fa6946072395196e0cf81e5333b33429
Marlon <marloncicero_araujo@hotmail.com>
2019-11-11 15:31:33 -0400

    update implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		
+		System.out.println("\n=== TEST 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
 	}
 
 }
